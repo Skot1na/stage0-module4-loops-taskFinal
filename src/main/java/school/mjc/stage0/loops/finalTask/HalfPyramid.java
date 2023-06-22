543bfe6c72111337523fa8eb1d35550c2dcf35dc
Kuanysh <amandos.kuanys@mail.ru>
2023-06-22 18:02:51 +0600

    M4_F

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int i, j;
+
+        for(i=0; i<cathetusLength; i++)
+        {
+            for(j=2*(cathetusLength-i); j>=0; j--)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+            for(j=0; j<=i; j++)
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        if (numberTableToPrint > 0) {
+            for (int i = 0; i <= 10; i++){
+                int num = i * numberTableToPrint;
+                System.out.println(num);
+            }
+        }
+
     }
 }
